edd5ae4c734cebc382dfb5c0417c8eba617c0f31
shunmaruko <t.shunryou@gmail.com>
2022-11-28 11:32:29 +0900

    revised logger

--- rootlogger.py
+++ rootlogger.py
@@ -3,7 +3,18 @@ import logging
 import time
 import sys
 
+from .paths import REPOSITORY_DIRECTORY 
+from .paths_helper import create_paths
+
 logger = logging.getLogger(__name__)
+
+class LogSettings:
+    def __init__(self, numeric_level):
+        self.__numeric_level = numeric_level
+
+    @property
+    def numeric_level(self) -> int:
+        return self.__numeric_level
 
 def _get_loglevel() -> int:
     """ get loglevel from command line argment
@@ -12,7 +23,9 @@ def _get_loglevel() -> int:
                            See https://docs.python.org/ja/3/library/logging.html?highlight=logging%20debug
         
     """
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter
+            )
     group=parser.add_mutually_exclusive_group()
     group.add_argument(
             "-l", 
@@ -35,36 +48,45 @@ def _get_loglevel() -> int:
     numeric_level= getattr(logging, args.log.upper(), None)
     return numeric_level 
 
-def setup_root_logger():
-    """ Setup root logger         
-    You can only change console log level by parsing -l or --log
-    For the detail, run with option -h 
-    """
-    logger = logging.getLogger()
-    numeric_level=_get_loglevel()
-    logger.setLevel(numeric_level)
-    
+def _add_stream_handler(settings: LogSettings):
     # create console handler and set level to debug
     ch = logging.StreamHandler()
-    ch.setLevel(numeric_level)
+    ch.setLevel(settings.numeric_level)
     # create formatter
     formatter = logging.Formatter('%(asctime)s: %(message)s')
     # add formatter to ch
     ch.setFormatter(formatter)
     # add Handeler to logeer 
-    logger.addHandler(ch)
+    logging.getLogger().addHandler(ch)
 
+def _add_file_handler(settings: LogSettings):
     # create file handler and set level to debug
-    logfile = "./log/" + time.strftime('%Y-%m-%d_%H:%M:%S') + '.log'
+    logfile = REPOSITORY_DIRECTORY.joinpath('logs').joinpath(time.strftime('%Y-%m-%d_%H:%M:%S')+'.log')
+    create_paths([logfile]) 
     fh = logging.FileHandler(filename=logfile)
     # create formatter
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
     # add ch to logger
-    logger.addHandler(fh)
+    logging.getLogger().addHandler(fh)
+
+def setup_handlers(settings: LogSettings):
+    """ Add Handlers to Root Loger         
+    You can only change console log level by parsing -l or --log
+    For the detail, run with option -h 
+    """
+    logging.getLogger().setLevel(settings.numeric_level)
+    _add_stream_handler(settings)
+    _add_file_handler(settings)
+
+def setup_root_logger():
+    settings = LogSettings(numeric_level=_get_loglevel())
+    setup_handlers(settings)
+    
 
 if __name__=="__main__":
     setup_root_logger()
+    # test 
     logger.critical('Started')
     logger.debug('debug message')
     logger.info('info message')
